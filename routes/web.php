0967ec244d994a7eb16eef29b7b8a6a770cca62b
Saw Maine K <sawmainek90@gmail.com>
2025-03-31 21:54:50 +0630

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::view('/{path?}', 'apitoolz::app');
+
+Route::get('/img/{path}', [FileStorageController::class, 'image'])->where('path', '.*');
+Route::get('/file/{path}', [FileStorageController::class, 'file'])->where('path', '.*');
