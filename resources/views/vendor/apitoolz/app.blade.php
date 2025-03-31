d8f7bb9123751525988773ede46f2069dcb12961
Saw Maine K <sawmainek90@gmail.com>
2025-03-31 21:32:49 +0630

    fixied-url

--- resources/views/vendor/apitoolz/app.blade.php
+++ resources/views/vendor/apitoolz/app.blade.php
@@ -10,7 +10,7 @@
     </style>
 		<link rel="preconnect" href="https://fonts.gstatic.com" />
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-    <script type="module" crossorigin src="/assets/index-xfFFgm6l.js"></script>
+    <script type="module" crossorigin src="/assets/index-BboZw9bD.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-D5HESF-r.css">
   </head>
   <body class="antialiased flex h-full text-base text-gray-700" style="overflow:visible !important; margin-right:0 !important; position:static !important;">
