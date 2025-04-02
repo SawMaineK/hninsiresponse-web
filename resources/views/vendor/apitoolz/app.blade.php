2f61ca0df59ddded8849c807d7fe688046c85e9b
Saw Maine K <sawmainek90@gmail.com>
2025-04-03 00:45:18 +0630

    fixed button

--- resources/views/vendor/apitoolz/app.blade.php
+++ resources/views/vendor/apitoolz/app.blade.php
@@ -10,8 +10,8 @@
     </style>
 		<link rel="preconnect" href="https://fonts.gstatic.com" />
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-    <script type="module" crossorigin src="/assets/index-BitmD3y9.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-oy6MS107.css">
+    <script type="module" crossorigin src="/assets/index-Dy5qgGwx.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-Cik9O0K5.css">
   </head>
   <body class="antialiased flex h-full text-base text-gray-700" style="overflow:visible !important; margin-right:0 !important; position:static !important;">
     <div class="flex grow" id="root"></div>
