2ec2f0324d49bac04115e2e0d1a2bd9d190f81ef
Saw Maine K <sawmainek90@gmail.com>
2025-04-03 22:06:10 +0630

    fixed marker and fivico

--- resources/views/vendor/apitoolz/app.blade.php
+++ resources/views/vendor/apitoolz/app.blade.php
@@ -1,19 +1,30 @@
 <!doctype html>
 <html class="h-full" lang="en" dir="ltr">
-  <head>
+
+<head>
     <meta charset="utf-8" />
-		<!-- <link rel="icon" href="/media/app/favicon.ico" /> -->
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/media/app/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
+    <meta name="description" content="မြန်မာ ငလျှင်ကြောင့် ပျောက်ဆုံးနေသူများ ထိခိုက်ဒဏ်ရာရသူများ သေဆုံးသူများ ရှာဖွေ ကယ်တင်နိုင်ရန်။
+    လှူမယ့်သူနဲ့ လိုအပ်မယ့်သူကြား ချိတ်ဆက်နိုင်ဖို့
+    ပျောက်တဲ့သူနဲ့ ရှာဖွေကယ်တင်မယ့် လူကြား ချိတ်ဆက်နိုင်ဖို့ သတင်းအချက်အလက် စုစည်းဖော်ပြပေးမယ့် web app တစ်ခုပါ။" />
+    <meta name="keywords" content="ငလျှင်, ကယ်ဆယ်ရေး, အကူအညီ, စစ်ကိုင်း, မန်းလေး" />
+    <meta property="og:image" content="/media/app/og-image.jpg" />
+    <meta property="og:image:alt" content="Hnin Si Response Team" />
+    <meta name="author" content="SixTech Team" />
     <style type="text/css">
 
     </style>
-		<link rel="preconnect" href="https://fonts.gstatic.com" />
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-    <script type="module" crossorigin src="/assets/index-DslynxvF.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-CeKOcg9-.css">
-  </head>
-  <body class="antialiased flex h-full text-base text-gray-700" style="overflow:visible !important; margin-right:0 !important; position:static !important;">
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+  <script type="module" crossorigin src="/assets/index-ClO7mjjW.js"></script>
+  <link rel="stylesheet" crossorigin href="/assets/index-CeKOcg9-.css">
+</head>
+
+<body class="antialiased flex h-full text-base text-gray-700"
+    style="overflow:visible !important; margin-right:0 !important; position:static !important;">
     <div class="flex grow" id="root"></div>
-  </body>
+</body>
+
 </html>
