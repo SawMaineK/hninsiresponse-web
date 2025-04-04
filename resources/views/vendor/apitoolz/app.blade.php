f5bb00035ae2ef7262df266a5db415825ae0069b
Saw Maine K <sawmainek90@gmail.com>
2025-04-04 23:07:09 +0630

    fix count on maps and share

--- resources/views/vendor/apitoolz/app.blade.php
+++ resources/views/vendor/apitoolz/app.blade.php
@@ -18,7 +18,7 @@
     </style>
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-  <script type="module" crossorigin src="/assets/index-DvgptwDe.js"></script>
+  <script type="module" crossorigin src="/assets/index-B8s3QR8V.js"></script>
   <link rel="stylesheet" crossorigin href="/assets/index-ek8wjOcZ.css">
 </head>
 
