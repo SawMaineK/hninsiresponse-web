73d136bd6790b252501f8f8ff336f19e05d9c3b3
Saw Maine K <sawmainek90@gmail.com>
2025-04-05 20:55:23 +0630

    fix social preview

--- resources/views/incident-detail.blade.php
+++ resources/views/incident-detail.blade.php
@@ -2,7 +2,7 @@
 @section('meta')
     <meta property="og:title" content="{{ $incident->title }}" />
     <meta property="og:description" content="{{ $incident->name }}" />
-    <meta property="og:image" content="{{ asset('img/' . $incident->photo->url) }}" />
+    <meta property="og:image" content="{{ asset('img/' . $incident->photo->url . '?w=400&h=400&fit=crop') }}" />
     <meta property="og:url" content="{{ url()->current() }}" />
     <meta property="og:type" content="website" />
     <meta name="twitter:card" content="summary_large_image" />
